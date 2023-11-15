af500287a3816dcb2c8c0db344badd51790d15f8
matildatilda <melty.bombom@gmail.com>
2023-11-15 12:53:38 +0000

    first commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>AI Weapon Shop</title>
+      </Head>
+
+      <main>
+        <div>
+            <h1>AI Weapon Shop</h1>
+            <p>Welcome to AI Weapon Shop! What are you looking for?<a href="demo.html">Start AI chat.</a></p>
+        </div>
+      </main>
+
+    </div>
+  );
+}
